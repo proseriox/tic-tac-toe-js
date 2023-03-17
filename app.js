0ad9b1a861c4d0b7ae9f5f985a26af3e984c8f6a
Seriox <sebastian.krzyzan@outlook.com>
2023-03-17 20:40:14 +0100

    add player names

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const statusMessage = document.querySelector('#status-message');
 const restartButton = document.querySelector('#restart-button');
 const playerXScore = document.querySelector('#player-x-score');
 const playerOScore = document.querySelector('#player-o-score');
+const playerXName = document.querySelector('#player-x-name');
+const playerOName = document.querySelector('#player-o-name');
+const namesForm = document.querySelector('#names-form');
+const playerXNameInput = document.querySelector('#player-x-name-input');
+const playerONameInput = document.querySelector('#player-o-name-input');
 const resetButton = document.querySelector('#reset-button');
 
 let currentPlayer = 'X';
@@ -10,6 +15,8 @@ let gameIsLive = true;
 let winner = null;
 let playerXPoints = 0;
 let playerOPoints = 0;
+let playerX = 'Player X';
+let playerO = 'Player O';
 
 const winningCombinations = [
   [0, 1, 2],
@@ -40,7 +47,7 @@ function checkGameStatus() {
 
   if (gameIsLive) {
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    statusMessage.textContent = `${currentPlayer}'s turn`;
+    statusMessage.textContent = `${getPlayerName(currentPlayer)}'s turn`;
   }
 }
 
@@ -54,7 +61,7 @@ function checkWin() {
       gameIsLive = false;
       winner = currentPlayer;
       highlightWinnerCells(combination);
-      statusMessage.textContent = `${winner} has won!`;
+      statusMessage.textContent = `${getPlayerName(winner)} has won!`;
       updateScore();
       break;
     }
@@ -82,27 +89,72 @@ function updateScore() {
     playerOPoints++;
     playerOScore.textContent = playerOPoints;
   }
+  localStorage.setItem('playerXPoints', playerXPoints);
+  localStorage.setItem('playerOPoints', playerOPoints);
 }
 
 function handleRestartClick() {
   currentPlayer = 'X';
   gameIsLive = true;
   winner = null;
-  statusMessage.textContent = `${currentPlayer}'s turn`;
+  statusMessage.textContent = `${getPlayerName(currentPlayer)}'s turn`;
 
   for (let cell of cells) {
     cell.classList.remove('X', 'O', 'winner');
     cell.textContent = '';
   }
+
+  updateNames();
 }
 
 function handleResetClick() {
   playerXPoints = 0;
   playerOPoints = 0;
-  playerXScore.textContent = playerXPoints;
-  playerOScore.textContent = playerOPoints;
-  localStorage.clear();
+  playerX = 'Player X';
+  playerO = 'Player O';
+  localStorage.removeItem('playerXPoints');
+  localStorage.removeItem('playerOPoints');
+  localStorage.removeItem('playerXName');
+  localStorage.removeItem('playerOName');
+  updateNames();
+  handleRestartClick();
 }
+
+function updateNames() {
+  playerXName.textContent = getPlayerName('X');
+  playerOName.textContent = getPlayerName('O');
+}
+
+function getPlayerName(player) {
+  if (player === 'X') {
+    return playerX;
+  } else if (player === 'O') {
+    return playerO;
+  } else {
+    return '';
+  }
+}
+
+function handleNamesFormSubmit(e) {
+  e.preventDefault();
+
+  const playerXNameInputValue = playerXNameInput.value.trim();
+  const playerONameInputValue = playerONameInput.value.trim();
+
+  if (playerXNameInputValue !== '') {
+    playerX = playerXNameInputValue;
+    localStorage.setItem('playerXName', playerX);
+  }
+
+  if (playerONameInputValue !== '') {
+    playerO = playerONameInputValue;
+    localStorage.setItem('playerOName', playerO);
+  }
+
+  updateNames();
+  handleRestartClick();
+}
+
 
 cells.forEach(cell => {
   cell.addEventListener('click', handleCellClick);
@@ -112,9 +164,13 @@ restartButton.addEventListener('click', handleRestartClick);
 
 resetButton.addEventListener('click', handleResetClick);
 
+namesForm.addEventListener('submit', handleNamesFormSubmit);
+
 window.addEventListener('load', () => {
   const storedPlayerXPoints = localStorage.getItem('playerXPoints');
   const storedPlayerOPoints = localStorage.getItem('playerOPoints');
+  const storedPlayerXName = localStorage.getItem('playerXName');
+  const storedPlayerOName = localStorage.getItem('playerOName');
 
   if (storedPlayerXPoints) {
     playerXPoints = parseInt(storedPlayerXPoints);
@@ -125,5 +181,15 @@ window.addEventListener('load', () => {
     playerOPoints = parseInt(storedPlayerOPoints);
     playerOScore.textContent = playerOPoints;
   }
-});
 
+  if (storedPlayerXName) {
+    playerX = storedPlayerXName;
+  }
+
+  if (storedPlayerOName) {
+    playerO = storedPlayerOName;
+  }
+
+  updateNames();
+  handleRestartClick();
+});
